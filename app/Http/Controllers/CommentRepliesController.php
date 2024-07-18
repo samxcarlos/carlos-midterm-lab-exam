697bc1fbd9ac58e4a9751d34d81acaaea93b74a1
samuel carlos <carlosxsamuel7262@gmail.com>
2024-07-18 11:04:55 +0800

    laravel model controller

---
+++ app/Http/Controllers/CommentRepliesController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\comment_replies;
+use Illuminate\Http\Request;
+
+class CommentRepliesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(comment_replies $comment_replies)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(comment_replies $comment_replies)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, comment_replies $comment_replies)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(comment_replies $comment_replies)
+    {
+        //
+    }
+}
